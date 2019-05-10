220071b97325c2162c0ad7ba7d1c44d847cdbeeb
廖伟 <24591106@qq.com>
2019-05-10 17:37:50 +0800

    Initial commit

---
+++ src/main/java/cn/net/liaowei/sc/consul/client/consumer/ConsumerController.java
@@ -0,0 +1,32 @@
+package cn.net.liaowei.sc.consul.client.consumer;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author LiaoWei
+ */
+@RestController
+@RequestMapping("/consumer")
+@Api(tags = "/consumer", description = "服务消费")
+public class ConsumerController {
+    private RestTemplate restTemplate;
+
+    public ConsumerController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/zone")
+    public Map<String, String> consumerZone() {
+        String name = restTemplate.getForObject("http://consul-client/zone/name", String.class);
+        Map<String, String> result = new HashMap<String, String>();
+        result.put("zone", name);
+        return result;
+    }
+}
---
+++ src/main/java/cn/net/liaowei/sc/consul/client/consumer/ScConsulClientConsumerApplication.java
@@ -0,0 +1,13 @@
+package cn.net.liaowei.sc.consul.client.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScConsulClientConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScConsulClientConsumerApplication.class, args);
+	}
+
+}
